f1213ed0651636e5f6cf90d25185e2557c7e332a
jeanramalho <jeanramalho.dev@gmail.com>
2022-09-01 12:53:06 -0300

    Comenta código

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+//configurações do particles js baixadas no codepen do site https://vincentgarreau.com/particles.js/
+//Crie as configurações no site https://vincentgarreau.com/particles.js/ e abra no codepen para baixa-las
+
 particlesJS("particles-js", {
     particles: {
       number: { value: 80, density: { enable: true, value_area: 800 } },
+      //muda a cor da particula
       color: { value: "#ffffff" },
       shape: {
+        //tipo da particula
         type: "circle",
+        //define tamanho e cor dos nós
         stroke: { width: 0, color: "#000000" },
         polygon: { nb_sides: 5 },
         image: { src: "img/github.svg", width: 100, height: 100 }
@@ -53,10 +59,13 @@ particlesJS("particles-js", {
     },
     retina_detect: true
   });
+
+  //variavel que monta mostrador de status e contador e partiulas
   var count_particles, stats, update;
   stats = new Stats();
   stats.setMode(0);
   stats.domElement.style.position = "absolute";
+  //feature adicionada para ocultar o mostrador de status
   stats.domElement.style.display = "none"
   stats.domElement.style.left = "0px";
   stats.domElement.style.top = "0px";
